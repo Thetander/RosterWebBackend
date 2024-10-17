a4c2e6e182e3d77357a795fea78f7baa8647138d
Thetander <joelzunta@hotmail.com>
2024-10-17 11:40:45 -0500

    updating repository

--- src/menu-role/aplications/services/menu-role.service.ts
+++ src/menu-role/aplications/services/menu-role.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { MenuRoleEntity } from 'src/menu-role/domain/entities/menu-role.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class MenuRoleService {}
+export class MenuRoleService {
+    constructor(
+        @InjectRepository(MenuRoleEntity)
+        private readonly MenuRoleEntityRepository: Repository<MenuRoleEntity>
+    ){}
+   async getMany(){}
+   async getOne(){}
+   async createOne(){}
+   async editOne(){}
+   async deleteOne(){}
+   
+}
